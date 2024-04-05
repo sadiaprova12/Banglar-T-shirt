bca8dea58b0f8a939ce090a4b6561476199a049d
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-06 02:18:27 +0600

    link to grandpa

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,7 @@ const Header = () => {
             <Link to="/review">Order Review</Link>
             <Link to="/about">CheckOut</Link>
             <Link to="/contact">Contact</Link>
+            <Link to="/grandpa">Grandpa</Link>
         </nav>
     );
 };
