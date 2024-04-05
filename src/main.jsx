a0f3d18e1a8190dfa1695ae70b1e2f3524ca40e1
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-06 02:18:13 +0600

    nested grandpa

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import {
 import Main from './components/Layout/Main.jsx';
 import Home from './components/Home/Home.jsx';
 import OrderReview from './components/OrderReview/OrderReview.jsx';
+import Grandpa from './components/Grandpa/Grandpa.jsx';
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
       {
         path: 'review',
         element: <OrderReview></OrderReview>
+      },
+      {
+        path: '/grandpa',
+        element: <Grandpa></Grandpa>
       }
     ]
   }
