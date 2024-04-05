fb30a06f138029ab228b86793e818a9df79afbb2
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-05 21:53:33 +0600

    added cart file

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+            <h2>Order Summary</h2>
+        </div>
+    );
+};
+
+export default Cart;
