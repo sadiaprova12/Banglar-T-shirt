20b67484c44c52b8032dac2ee69dbf91c7c5efec
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-05 21:04:06 +0600

    react router setup

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+const Home = () => {
+    const tshirts = useLoaderData();
+    return (
+        <div>
+            <h2>This is home: {tshirts.length}</h2>
+        </div>
+    );
+};
+
+export default Home;
