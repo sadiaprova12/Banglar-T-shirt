9710c3fec1b80e26f3de2f884f28146e8b964988
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-05 21:54:41 +0600

    import tshirt cart and home css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,24 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css';
 const Home = () => {
     const tshirts = useLoaderData();
     return (
-        <div>
-            <h2>This is home: {tshirts.length}</h2>
+        <div className='home-container'>
+           <div className='t-shirts-container'>
+           {
+            tshirts.map(tshirt => <Tshirt
+            key={tshirt._id}
+            tshirt={tshirt}
+            ></Tshirt>)
+           }
+           </div>
+           <div className="cart-container">
+            <Cart></Cart>
+           </div>
         </div>
     );
 };
