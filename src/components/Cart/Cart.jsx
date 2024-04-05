cd4737ffee1fe768ee4e33d52ddc65408e52f996
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-06 00:32:34 +0600

    conditional use if or if else to set a variable that will contain an element, components

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,9 +4,14 @@ import React from 'react';
 
 // eslint-disable-next-line react/prop-types
 const Cart = ({cart, handleRemoveFromCart}) => {
+    let message;
+    if(cart.length === 0){
+        message = <p>Please add some products</p>
+    }
     return (
         <div>
             <h2>Order Summary: {cart.length}</h2>
+            {message}
             {
                 cart.map(tshirt => <p 
                     key={tshirt._id}
@@ -19,3 +24,19 @@ const Cart = ({cart, handleRemoveFromCart}) => {
 };
 
 export default Cart;
+
+
+/**
+ * CONDITIONAL RENDERING
+ * 1. use if or if else to set a variable that will contain an element, components
+ * 2. ternary operator: condition ? 'for true' : 'false'
+ * 3. Logical &&: (if the condition is true then the next thing will be displayed)
+ * 4. Logical ||: (if the condition is false then the next thing will be displayed)
+ * 
+ * */ 
+
+/**
+ * CONDITIONAL CSS Class
+ * 1. use ternary 
+ * 2. ternary inside template string
+ * */ 
