82be177c5b69b894db66af39cabf322ee20bdb45
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-06 02:29:26 +0600

    added aunty

---
+++ src/components/Aunty/Aunty.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Aunty = () => {
+    return (
+        <div>
+            <h2>Aunty</h2>
+        </div>
+    );
+};
+
+export default Aunty;
