1be18a2440aa64d0fef37a7de5e0999c9606cc7c
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-05 22:14:02 +0600

    added btn and onclick

--- src/components/Tshirt/Tshirt.jsx
+++ src/components/Tshirt/Tshirt.jsx
@@ -1,11 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './Tshirt.css'
-const Tshirt = ({tshirt}) => {
+// eslint-disable-next-line react/prop-types
+const Tshirt = ({tshirt, handleAddToCart}) => {
+    // eslint-disable-next-line react/prop-types
     const {picture,name,price} =tshirt;
     return (
         <div className='t-shirt'>
             <img src={picture} alt="" />
+            <h4>{name}</h4>
+            <p>Price: ${price} </p>
+            <button onClick={() => handleAddToCart (tshirt)}>Buy Now</button>
         </div>
     );
 };
