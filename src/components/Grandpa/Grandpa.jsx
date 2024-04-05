8c4b5d04b03dc2b4e8d6a5345f1495c709b9b7af
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-06 02:30:27 +0600

    added section

---
+++ src/components/Grandpa/Grandpa.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Father from '../Father/Father';
+import Uncle from '../Uncle/Uncle';
+import Aunty from '../Aunty/Aunty';
+import './Grandpa.css'
+const Grandpa = () => {
+    return (
+        <div className='grandpa'>
+            <h2>Grandpa</h2>
+            <section className='flex'>
+                <Father></Father>
+                <Uncle></Uncle>
+                <Aunty></Aunty>
+            </section>
+        </div>
+    );
+};
+
+export default Grandpa;
